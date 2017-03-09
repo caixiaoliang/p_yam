cb26cd5427b1e1b2ff2417b1dfbe380a11fcbff6
chengxiyue <xiaoliangcaig@gmail.com>
2017-03-09 18:33:52 +0800

    fix fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,9 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths <<  Rails.root.join("app", "assets", "fonts")
+# Rails.application.config.assets.paths <<  Rails.root.join("app", "assets", "fonts")
+# Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
