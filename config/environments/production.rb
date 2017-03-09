82bee0f55a082eb74543765181faf4f708994753
chengxiyue <xiaoliangcaig@gmail.com>
2017-03-09 23:27:31 +0800

    fix mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,22 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-    location: '/usr/sbin/sendmail',
-    arguments: '-i -t'
-  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'xiaoliangcaig@gmail.com',
+  password:             'chengxiyue',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_options = {from: 'no-reply@p_yam.com'}
+  # config.action_mailer.default_options = {from: 'no-reply@p_yam.com'}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
