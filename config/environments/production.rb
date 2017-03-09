4f71492360e0024ba154cb37c6af06e191e9c99a
chengxiyue <xiaoliangcaig@gmail.com>
2017-03-09 22:50:01 +0800

    send mail by default sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
-  config.action_mailer.delivery_method = :smtp
-  # Defaults to:
-  config.action_mailer.smtp_settings = {
-    user_name:           "root",
-    password:            "Kembo2017",
-    location: '/usr/sbin/sendmail',
-    arguments: '-i -t'
-  }
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.default_options = {from: 'no-reply@p_yam.com'}
   # Code is not reloaded between requests.
   config.cache_classes = true
